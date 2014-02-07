a2261094f3537d73eee4dff6fd6bd95dede966f5
develpr <kevin@develpr.com>
2014-02-07 07:44:30 -0800

    Order items and order payment added

--- app/models/laragento/OrderItem.php
+++ app/models/laragento/OrderItem.php
@@ -6,8 +6,30 @@ use \Eloquent;
 class OrderItem extends Eloquent {
 
 	protected $table = 'sales_flat_order_item';
-	protected $primaryKey = 'entity_id';
+	protected $primaryKey = 'item_id';
 
 	//todo: need to complete this, including build json response object and relationship to product (?)
+
+	public function order()
+	{
+		return $this->hasOne('Laragento\Order', 'entity_id');
+	}
+
+
+	public function prepareOutput($apiVersion)
+	{
+
+		$return = array(
+			'id' => $this->entity_id,
+			'href' => $this->path = self::uri($apiVersion, $this->entity_id),
+			'status' => $this->status,
+			'state' => $this->state,
+			'customerId' => $this->customer_id,
+			'customerUri' => Customer::uri($apiVersion, $this->customer_id),
+			'discountAmount' => $this->base_discount_amount,
+		);
+
+		return $return;
+	}
 
 }
